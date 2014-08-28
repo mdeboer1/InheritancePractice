5848048bbd5a366d22909eb8186b82c1f29e7171
markr_000 <markr_000@Jet>
2014-08-28 14:40:26 -0500

    Created classes in concrete package

--- src/my/concrete/Employee.java
+++ src/my/concrete/Employee.java
@@ -12,5 +12,65 @@ package my.concrete;
  */
 public class Employee {
     
-    private String name;
+    private String name, hireDate, address, dateOfBirth, SSN, phoneNum;
+    
+    public Employee (String name, String hireDate, String address, String dateOfBirth, 
+            String SSN, String phoneNum){
+        this.name = name;
+        this.hireDate = hireDate;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.SSN = SSN;
+        this.phoneNum = phoneNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+    
+    
 }
